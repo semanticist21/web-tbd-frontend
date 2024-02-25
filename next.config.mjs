fe6d481e02bb2bc9c338d29f96a823276f7ff5ed
jiwon <semanticist0@gmail.com>
2024-02-25 17:04:56 +0900

    🔧 eslint & routes

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
-    // 아래를 추가합니다.
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
---
+++ src/routes/routes.ts
@@ -0,0 +1,5 @@
+export const ROUTE = {
+  LOGIN: '/login',
+  SIGN_UP: 'user/sign-up',
+  FORGOT_PASSWORD: '/user/forgot-password',
+} as const
